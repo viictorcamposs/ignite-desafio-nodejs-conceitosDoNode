8da587c003bf657415cc23b4e09b59e73b80709e
viictorcamposs <campos.viictor@gmail.com>
2021-03-12 13:15:01 -0300

    feat: criar conta de usuario

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+const { v4: uuid } = require('uuid');
 
-// const { v4: uuidv4 } = require('uuid');
+const app = express()
+.use(cors())
+.use(express.json());
 
-const app = express();
+const users = [];
 
-app.use(cors());
-app.use(express.json());
-
-// const users = [];
-
-function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+const checksExistsUserAccount = (request, response, next) => {
+ // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const userAlreadyExists = users.find(
+    (user) => user.username === username
+  );
+
+  if (userAlreadyExists) return response.status(400).json({
+    error: "Error while we tried to create user with this username."
+  });
+
+  users.push({
+    id: uuid(),
+    name,
+    username,
+    todos: [],
+  });
+
+  return response.status(201).send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
